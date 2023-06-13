9f0351495a8a1036c31b15077eebfb67bafca438
Tanchev,Hristo H <h.tanchev@student.fontys.nl>
2023-06-14 00:45:42 +0200

    test

--- client/src/components/PIN.js
+++ client/src/components/PIN.js
@@ -1,79 +1,9 @@
-// import React, { useRef, useEffect } from "react";
-
-
-// import '../components/componentsCSS/PIN.css'
-// const PIN = () => {
-//   const inputsRef = useRef([]);
-//   const buttonRef = useRef(null);
-
-//   useEffect(() => {
-//     inputsRef.current[0].focus();
-//   }, []);
-
-//   const handleInput = (e, currentIndex) => {
-//     const currentInput = inputsRef.current[currentIndex];
-//     const nextInput = inputsRef.current[currentIndex + 1];
-//     const prevInput = inputsRef.current[currentIndex - 1];
-
-//     if (currentInput.value.length > 1) {
-//       currentInput.value = "";
-//       return;
-//     }
-
-//     if (nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== "") {
-//       nextInput.removeAttribute("disabled");
-//       nextInput.focus();
-//     }
-
-//     if (e.key === "Backspace") {
-//       inputsRef.current.forEach((input, index) => {
-//         if (currentIndex <= index && prevInput) {
-//           input.setAttribute("disabled", true);
-//           input.value = "";
-//           prevInput.focus();
-//         }
-//       });
-//     }
-
-//     if (!inputsRef.current[3].hasAttribute("disabled") && inputsRef.current[3].value !== "") {
-//       buttonRef.current.classList.add("active");
-//       return;
-//     }
-
-//     buttonRef.current.classList.remove("active");
-//   };
-
-//   return (
-    
-//     <div className="containerPin">
-      
-//       <form>
-//         <div className="input-field">
-//           <input type="text" maxLength="1" ref={(el) => (inputsRef.current[0] = el)} onKeyUp={(e) => handleInput(e, 0)} />
-//           <input type="text" maxLength="1" disabled ref={(el) => (inputsRef.current[1] = el)} onKeyUp={(e) => handleInput(e, 1)} />
-//           <input type="text" maxLength="1" disabled ref={(el) => (inputsRef.current[2] = el)} onKeyUp={(e) => handleInput(e, 2)} />
-//           <input type="text" maxLength="1" disabled ref={(el) => (inputsRef.current[3] = el)} onKeyUp={(e) => handleInput(e, 3)} />
-//         </div>
-        
-//         <button className="pinbutton" type="submit" disabled ref={buttonRef} ><a href="/setusername" style={{textdecoration:'none', color:'black',  textDecoration: 'none'}}>
-//           Verify
-//        </a> </button>
-       
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default PIN;
-
-
-
-
 
 
 import React, { useRef, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import baseUrl from "../baseUrl";
 import '../components/componentsCSS/PIN.css';
 
 const PIN = () => {
@@ -122,7 +52,7 @@ const PIN = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get(`/api/code/${code}`);
+      const response = await axios.get(`${baseUrl}/api/code/${code}`);
       const activeQuiz = response.data;
       // Redirect to the appropriate page based on the active quiz data
       if (activeQuiz) {
