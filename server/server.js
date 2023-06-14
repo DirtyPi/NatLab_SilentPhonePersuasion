eaa290a7c54eed025d016837323dbfe1cf9b4439
Tanchev,Hristo H <h.tanchev@student.fontys.nl>
2023-06-14 18:32:14 +0200

    attempt to fetch data to production part 2

--- server/server.js
+++ server/server.js
@@ -1,3 +1,51 @@
+// require(`dotenv`).config()
+// const express = require("express");
+// const quizRouts = require('../server/routes/quizRoutes')
+// const activeQuizRouts = require('../server/routes/activeQuizRoutes')
+// const mongoose = require('mongoose')
+// const path = require("path")
+
+// //express app
+// const app = express()
+
+// //middleware
+// app.use(express.json())
+// // // Enable CORS
+// // app.use(cors());
+
+// app.use((req,res,next) => {
+//     console.log(req.path, req.method)
+//     next()
+// })
+
+// //routes
+// app.use('/api/quiz', quizRouts)
+// app.use('/api/active/quiz', activeQuizRouts)
+
+// //connect to db
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(() => {
+//         //listen for req
+//         app.listen(process.env.PORT, () =>{
+//             console.log('Conncected', process.env.PORT)
+//         })
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     })
+
+//     //production script
+//  // Serve static files from the client build directory
+// const clientBuildPath = path.join(__dirname, '../client/build');
+// app.use(express.static("../client/build"));
+
+// // Serve the index.html file for all other routes
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '..','client' ,'build', 'index.html'));
+// });
+
+
+
 require(`dotenv`).config()
 const express = require("express");
 const quizRouts = require('../server/routes/quizRoutes')
@@ -8,12 +56,8 @@ const path = require("path")
 //express app
 const app = express()
 
-
-
 //middleware
 app.use(express.json())
-// // Enable CORS
-// app.use(cors());
 
 app.use((req,res,next) => {
     console.log(req.path, req.method)
@@ -24,27 +68,23 @@ app.use((req,res,next) => {
 app.use('/api/quiz', quizRouts)
 app.use('/api/active/quiz', activeQuizRouts)
 
+// Serve static files from the client build directory
+const clientBuildPath = path.join(__dirname, '../client/build');
+app.use(express.static(clientBuildPath));
+
+// Serve the index.html file for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientBuildPath, 'index.html'));
+});
+
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         //listen for req
         app.listen(process.env.PORT, () =>{
-            console.log('Conncected', process.env.PORT)
+            console.log('Connected', process.env.PORT)
         })
     })
     .catch((error) => {
         console.log(error)
     })
-
-    //production script
- // Serve static files from the client build directory
-const clientBuildPath = path.join(__dirname, '../client/build');
-app.use(express.static("../client/build"));
-
-// Serve the index.html file for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..','client' ,'build', 'index.html'));
-});
-
-
-
