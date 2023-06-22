2fdb8225d022cad315fac5ccd87bbec90cfe1b7c
Tanchev,Hristo H <h.tanchev@student.fontys.nl>
2023-06-22 08:51:53 +0200

    cors update

--- server.js
+++ server.js
@@ -97,44 +97,50 @@
 // //     );
 // //     next();
 // // });
-
-require(`dotenv`).config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const quizRoutes = require('./routes/quizRoutes');
 const activeQuizRoutes = require('./routes/activeQuizRoutes');
 const mongoose = require('mongoose');
-const path = require("path");
-const cors = require("cors");
+const cors = require('cors');
 
-//express app
+// Express app
 const app = express();
 
-//middleware
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-app.use((req,res,next) => {
-    console.log(req.path, req.method);
-    next();
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
 app.use(function (err, req, res, next) {
-    console.error(err.stack); // log the error stack trace for debugging
-    res.status(500).json({ message: err.message }); // send the error message to the client
+  console.error(err.stack); // Log the error stack trace for debugging
+  res.status(500).json({ message: err.message }); // Send the error message to the client
 });
 
-//routes
+// Enable CORS for the Vercel domain
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://nat-lab-silent-phone-persuasion-react-git-main-dirtypi.vercel.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// Routes
 app.use('/api/quiz', quizRoutes);
 app.use('/api/active/quiz', activeQuizRoutes);
 
-//connect to db
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        //listen for req
-        app.listen(process.env.PORT, () =>{
-            console.log('Connected', process.env.PORT)
-        });
-    })
-    .catch((error) => {
-        console.log(error);
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('Connected', process.env.PORT);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
